283d003383ab3521dbac1d5b59471d596af78bba
LuMike <LuMike@users.noreply.github.com>
2022-04-04 09:12:29 +0630

    push test

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+//second commit
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
